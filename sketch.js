f060015e51967097c152aacb7f3c1cc81a392339
delishadalu <daludelisha@gmail.com>
2021-06-05 16:12:53 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -35,6 +35,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform2= new Ground(100,300,300,170)
+
 }
 
 function draw(){
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform2.display()
 }
